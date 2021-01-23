908a5431d151ef02c44b162c1c71df9bc06f1b66
natashacmyers <natashacmyers@gmail.com>
2021-01-23 19:54:09 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
-     $('.collapsible').collapsible();
-     $('.tooltipped').tooltip();
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format : "dd mmmm, yyyy",
+        yearRange : 3,
+        showClearBtn : true,
+        i18n : {
+            done: "Select"
+        }
+    });
   });
 
 
